040b7f0c3fb6923c8dc630ab66d2d9e3e9af6802
Emma Hartman <emmajhartman@gmail.com>
2020-01-27 11:03:32 -0500

    Feature: can query the directions api

--- App.js
+++ App.js
@@ -4,10 +4,35 @@ import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, Dimensions, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import {googleMapsApiKey} from'./secrets'
+// import google from ''
 
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
 
 import AppNavigator from './navigation/AppNavigator';
+
+async function getDirections(startLoc, destinationLoc) {
+  try {
+       const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&mode=transit&key=${googleMapsApiKey}`);
+       const respJson = await resp.json();
+       console.log(respJson)
+       if (respJson.routes.length > 0) {
+           const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+           const coords = points.map((point, index) => {
+               return {
+                   latitude: point[0],
+                   longitude: point[1],
+               };
+           });
+          //  this.setState({ coords });
+          console.log(coords)
+       }
+       return;
+   } catch (error) {
+       alert(error);
+   }
+}
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -21,16 +46,27 @@ export default function App(props) {
       />
     );
   } else {
+    getDirections("Disneyland","Universal+Studios+Hollywood")
+    //console.log(typeof PROVIDER_GOOGLE)
+    //let directionsService = new google.maps.DirectionsService()
     return (
       <View style={styles.container}>
         <MapView
           style={styles.mapStyle}
           provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: 40.705086,
+            longitude: -74.009151,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
         />
+        <AppNavigator />
       </View>
       // <View style={styles.container}>
       //   {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      //   <AppNavigator />
+        // <AppNavigator />
       // </View>
     );
   }
